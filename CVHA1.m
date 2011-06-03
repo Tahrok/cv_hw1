3fd61a2b1a897fee36679fa78dc243ecf22795aa
mike <masterhelix@web.de>
2011-06-03 11:59:30 +0200

    Kommentare + 2.3 angefangen

--- CVHA1.m
+++ CVHA1.m
@@ -3,13 +3,22 @@
 % Clemens Horch, ch@tum.de, Matrikelnummer: 3013692
 % Kinan Mahdi, kinan.mahdi@tum.de, Matrikelnummer: 
 
-%Für die letztendlich Abgabe bitte das folgende auskommentieren, und korrekte
-%Parameter Ihres Harris Detektors übergeben
+%Fï¿½r die letztendlich Abgabe bitte das folgende auskommentieren, und korrekte
+%Parameter Ihres Harris Detektors ï¿½bergeben
 Image = imread('Bilder/haus.png');
 IGray = rgb_to_gray(Image);
 [Fx,Fy]=sobel_xy(IGray);
-%tic
-Merkmale = harris_detektor(IGray,7,0.04,1000,1);
-%toc
 
-%Merkmale = fail(IGray,7,0.06,500000000000,1);
+% Werte initialisieren
+tile_size=30;
+N=5;
+min_dist=5;
+
+tic
+%Merkmale = harris_detektor(IGray,7,0.04,1000,1);
+%Merkmale=harris_detektor(Bild,W,k,tau,tile_size,N,min_dist,do_plot)
+Merkmale = harris_detektor(IGray,7,0.04,1500,tile_size,N,min_dist,1);
+t=toc;
+
+% Rechenzeit ausgeben
+fprintf('Harris Berechnungszeit: %.2fs\n', t)
--- harris_detektor.m
+++ harris_detektor.m
@@ -4,20 +4,33 @@
 % Kinan Mahdi, kinan.mahdi@tum.de, Matrikelnummer:
 
 %HARRIS_DETEKTOR
-function Merkmale=harris_detektor(Bild, W, k, tau, do_plot)
-%function Merkmale=harris_detektor(Bild, W, k, tau, tile_size, N, min_dist, do_plot)
+%function Merkmale=harris_detektor(Bild, W, k, tau, do_plot)
+function Merkmale=harris_detektor(Bild, W, k, tau, tile_size, N, min_dist, do_plot)
+% Check: Bild = Grauwertbild?
 if size(Bild,3) == 3
     Bild = rgb_to_gray(Bild);
 end
 
+% Check: tile_size quadratisch?
+if size(tile_size,2) == 2
+    tile_width=tile_size(1);
+    tile_height=tile_size(2);
+else
+    tile_width=tile_size;
+    tile_height=tile_size;
+end
+
+% Sobelfilter auf Bild anwenden
 [Fx,Fy]=sobel_xy(Bild);
 
+% Speicherplatz reservieren, MEHR SPEED! Yeah!
 G=zeros(2,2);
 windowX=zeros(W,W);
 windowY=zeros(W,W);
 windowXY=zeros(W,W);
 i=1;
 
+% Kantenbilder schon vorher quadrieren und nicht innerhalb der for-Schleife, MEHR SPEED! Yeah!
 Fx2=Fx.^2;
 Fy2=Fy.^2;
 FxFy=Fx.*Fy;
@@ -26,66 +39,56 @@ W=(W-1)/2;
 
 for r=W+1:size(Bild,1)-W
     for c=W+1:size(Bild,2)-W
+        % W*W Fenster aus quadriertem Kantenbild "ausschneiden"
         windowX=Fx2(r-W:r+W,c-W:c+W);
         windowY=Fy2(r-W:r+W,c-W:c+W);
         windowXY=FxFy(r-W:r+W,c-W:c+W);
+        
+        % G-Matrix berechnen
         G(1,1)=sum(windowX(:));
         G(1,2)=sum(windowXY(:));
         G(2,1)=G(1,2);
         G(2,2)=sum(windowY(:));
 
-        % Option 1:
+        % Option 1: Singulärwertbetrachtung
         d=eig(G);
         C=sqrt(min(d(:)));        
         
-        % Option 2:
+        % Option 2: alternative Kostenfunktion
         %C=det(G)-k*(trace(G))^2;
         
         if C > tau
-            Merkmale(i,:)=[r c];
+            Merkmale(i,:)=[r c C];
             i=i+1;
         end
     end
 end
 
-% for r=W+1:size(Bild,1)-W
-%     for c=W+1:size(Bild,2)-W
-%         windowX=Fx(r-W:r+W,c-W:c+W);
-%         windowY=Fy(r-W:r+W,c-W:c+W);
-%         G(1,1)=sum(sum(windowX*windowX));
-%         G(1,2)=sum(sum(windowX*windowY));
-%         G(2,1)=G(1,2);
-%         G(2,2)=sum(sum(windowY*windowY));
-%
-%         C=det(G)-k*trace(G)^2;
-%         if C > tau
-%            Merkmale(i,:)=[r c];
-%            i=i+1;
-%         end
-%         sigma2(i)=C;
-%         i=i+1;
 
+n=floor(size(Bild,1)/tile_height); % ganzzahlige Anzahl der Fenster, die in die Bildhöhe passen
+%rest_height=mod(size(Bild,1),tile_height)-1; % Bildhöhe - i*(Anzahl Fester)
+m=floor(size(Bild,2)/tile_width);
+%rest_width=mod(size(Bild,2),tile_width)-1; 
 
-%         d=eig(G);
-%         v=sqrt(min(d(:)));
-%         if v > tau
-%             sigma2(i)=v;
-%             Merkmale(i,:)=[r c];
-%             i=i+1;
-%         end
-
-% end
-% end
+for r=1:tile_height:n*tile_height
+    for c=1:tile_width:m*tile_width
+        [v]=find((Merkmale(:,1) >= r) & (Merkmale(:,1) < r+tile_height) & (Merkmale(:,2) >= c) & (Merkmale(:,2) < c+tile_width));
+        if size(v,1) == 0 % keine Merkmalspunkte innerhalb des Fensters gefunden
+            fprintf('Keine Merkmalspunkte innerhalb des Fensters!\n')   
+        else
+            fprintf('Merkmalspunkt: %i %i in Fenster %i bis %i und %i bis %i gefunden\n', Merkmale(v(1),1), Merkmale(v(1),2), r, r+tile_height, c, c+tile_width)
+        end
+        
+    end
+end
 
 
 
+fprintf('Anzahl gefundener Merkmale: %i\n',size(Merkmale,1))
+
+% Bild mit gefundenen Merkmalen plotten
 if do_plot==1
-    %     min(sigma2(:))
-    %     max(sigma2(:))
-    %     mean(sigma2(:))
-    %     size(sigma2)
-    size(Merkmale)
-
+    %size(Merkmale)
     imshow(Bild)
     hold on
 
@@ -93,7 +96,6 @@ if do_plot==1
     y=Merkmale(:,1);
     
     plot(x,y,'bx');
-    
 
     hold off
 
